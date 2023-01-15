9168a03a67d2b8f3ace7070121fbf95371f9212d
Manjorstreep <Manjorstreep@gmail.com>
2023-01-15 13:14:01 -0400

    bundle for android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'client-fastworld',
+  appId: 'app.fastworld.conductor',
+  appName: 'Fastworld Conductores',
   webDir: 'www',
   bundledWebRuntime: false
 };
